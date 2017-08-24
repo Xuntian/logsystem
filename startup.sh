e309868a3acfc24fd8dc569aabb77f4afe006e4d
李志强 <li.zq@foxmail.com>
2017-08-24 16:07:47 +0800

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -2,4 +2,4 @@
 
 workdir=`pwd`
 
-npl -d bootstrapper="script/apps/WebServer/WebServer.lua" port="8099" root="www/" dev="${workdir}" servermode="true"
+npl bootstrapper="script/apps/WebServer/WebServer.lua" port="8099" root="www/" dev="${workdir}" servermode="true"
